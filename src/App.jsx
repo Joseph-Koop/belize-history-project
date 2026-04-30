3434eb24df35374f8e0b1aa3de4f308bd89b4be5
Joseph Koop <2023159207@ub.edu.bz>
2026-04-30 12:17:07 -0600

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="">
+        <div>
+          <h1>
+            Belize History Project
+          </h1>
+          <h3>
+            Nationalist Movement
+          </h3>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
