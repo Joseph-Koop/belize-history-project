083a0d9e9dfafdd98fc269db7652474401f7bf44
Joseph Koop <2023159207@ub.edu.bz>
2026-05-02 11:36:35 -0600

    Basic working version

---
+++ data/references.js
@@ -0,0 +1,30 @@
+const references = [
+
+    "BBNTEAM. (2024, October 15). The PUP Split of 1956: What if…?? Belize News and Opinion on Www.breakingbelizenews.com. https://www.breakingbelizenews.com/2024/10/15/the-pup-split-of-1956-what-if/",
+
+    "Belize - Internal Self-Rule, 1964-81. (2026). Countrystudies.us. https://countrystudies.us/belize/69.htm",
+
+    "CELEBRATING 50 YEARS AFTER ADULT SUFFRAGE Civic Center, Belize City Presentation by I. Myrtle Palacio. (2004). https://www.belize-glessimaresearch.org/wp-content/uploads/bskpdf-manager2020/02/Celebrating-50-Years-after-Adult-Suffrage.pdf",
+
+    "History of the Legislature of Belize. (n.d.). National Assembly.https://www.nationalassembly.gov.bz/history-of-the-legislature-of-belize/",
+
+    "PUP - People’s United Party Belize. (n.d.). Www.pup.bz. https://www.pup.bz/about-pup/",
+
+    "People’s United Party, Belize’s oldest political party, turns 58 | Amandala Newspaper. (2008,September 30). Amandala.com.bz. https://amandala.com.bz/news/peoples-united-partybelizes-oldest-political-party-turns-58/",
+
+    "Some of the PUP founding officials and members, early 1950’s. Also a little history about The People’s United Party including Thistle Hall. (n.d.). Ambergriscaye.com.https://ambergriscaye.com/photogallery/210113.html",
+
+    "SBelize.com. (2023, August 28). Belize: The struggle for independence. https://belize.com/belize-the-struggle-for-independence/",
+
+    "Organization of American States. (1968, April 29). The Webster Proposals. https://www.oas.org/sap/peacefund/belizeandguatemala/timelinedocuments/1968april29websterproposals.pdf",
+
+    "Organization of American States. (1981, September 21). September 21 1981 Belize independence. https://www.oas.org/sap/peacefund/belizeandguatemala/timelinedocuments/September%2021%201981%20Belize%20Independence.pdf",
+
+    "United Nations General Assembly. (1975, December 8). Resolution 3432 (XXX): Question of Belize. https://digitallibrary.un.org/record/191144",
+
+    "United Nations General Assembly. (1980, November 11). Resolution 35/20: Question of Belize. https://digitallibrary.un.org/record/17223",
+
+];
+
+
+export default references;
--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,51 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import data from '../data/data.js'
+import { SquareArrowRight, SquareArrowLeft } from 'lucide-react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [current, setCurrent] = useState(0);
+
+  function navigate(index){
+    setCurrent(index);
+  }
 
   return (
     <>
-      <section id="">
-        <div>
-          <h1>
+      <div className="bg-gray-950 h-screen w-full flex flex-col justify-start text-white">
+        <section className="w-auto p-3 m-3">
+          <h1 className="text-[50px] text-center text-blue-600">
             Belize History Project
           </h1>
-          <h3>
-            Nationalist Movement
+          <h3 className="text-[20px] text-center">
+            Belize Nationalist Movement
           </h3>
-        </div>
-      </section>
+        </section>
+        <section className="w-auto flex justify-around m-3 rounded-full border border-blue-700">
+          {data.map((event, index) => (
+            <div key={index} className={`flex justify-center items-center h-10 w-20 rounded-full hover:cursor-pointer hover:bg-blue-800 ${current == index ? 'bg-blue-700' : ''}`} onClick={() => navigate(index)}>
+              {event.date}
+            </div>
+          ))}
+        </section>
+        <section className="w-autojustify-center p-9 m-3 flex flex-wrap gap-10 bg-gray-900 rounded-2xl">
+          <div>
+            <img src={data[current].image} alt={data[current].name} className="max-w-100" />
+          </div>
+          <div className="grid grid-cols-1 gap-10 max-w-300">
+            <div className="flex items-center gap-10">
+              <h3 className="text-red-900 text-5xl">{data[current].date}</h3>
+              <h3 className="text-red-800 text-3xl">{data[current].name}</h3>
+            </div>
+            <p>{data[current].description}</p>
+          </div>
+        </section>
+        <section className="w-auto flex justify-end p-3 m-3 gap-3">
+          <SquareArrowLeft className="hover:text-blue-600 hover:cursor-pointer size-8" onClick={current !== 0 ? () => navigate(current - 1) : null}/>
+          <SquareArrowRight className="hover:text-blue-600 hover:cursor-pointer size-8" onClick={current !== (data.length - 1) ? () => navigate(current + 1) : null}/>
+        </section>
+      </div>
     </>
   )
 }
